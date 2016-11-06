1afea4f91a3179d7fafac4ca1470c25e2e0679fa
Dennis Shtatnov <densht@gmail.com>
2016-11-06 17:46:12 -0500

    Reorganizing poly code for minsnap impl

--- src/trajectory/polynomial.cpp
+++ src/trajectory/polynomial.cpp
@@ -1,35 +1,5 @@
 #include <tansa/trajectory.h>
-
-
-// Computes a derivative based on the 0th order evaluation
-// So given [t^0, t^1, t^2, t^3, t^4 ...]
-// Computes [0, 1*t^0, 2*t^1, 3*t^2, ...   ] for n = 1
-inline VectorXd diffvec(const VectorXd t, int n) {
-
-	VectorXd dt = VectorXd::Zero(t.size());
-
-	for(int i = n; i < t.size(); i++) {
-		float c = 1;
-		for(int p = i; p > i - n; p--) {
-			c *= p;
-		}
-
-		dt(i) = c * t(i - n);
-	}
-
-	return dt;
-}
-
-// Given t, computes [t^0, t^1, t^2, ...t^(n-1)]
-inline VectorXd powvec(double t, int n) {
-	VectorXd v(n);
-	v(0) = 1;
-	for(int i = 1; i < n; i++) {
-		v(i) = v(i - 1) * t;
-	}
-
-	return v;
-}
+#include "utils.h"
 
 
 PolynomialTrajectory::PolynomialTrajectory(const VectorXd c[], double t1, double t2)

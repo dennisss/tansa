1cd8a16519a65b37dc0333fef67baf994dc09571
tessmichi <tess.distefano@gmail.com>
2016-11-17 02:19:49 -0500

    Add header for breakpoints, reformat breakpoints in jocs

---
+++ include/tansa/breakpoint.h
@@ -0,0 +1,24 @@
+//
+// Created by tess on 11/17/16.
+//
+
+#ifndef TANSA_BREAKPOINT_H
+#define TANSA_BREAKPOINT_H
+
+#include <memory>
+
+namespace tansa {
+
+typedef unsigned breakpointId;
+
+class Breakpoint {
+
+public:
+
+private:
+
+
+};
+}
+
+#endif //TANSA_BREAKPOINT_H

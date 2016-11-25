a7f46eb802a09efc51d35d796bf942bf4fc6ba99
tessmichi <tess.distefano@gmail.com>
2016-11-25 01:54:15 -0500

    Fix order of values in strobe; fix bps to be 46 'cycles' total rather than per second

--- src/trajectory/strobe.cpp
+++ src/trajectory/strobe.cpp
@@ -5,9 +5,8 @@ double StrobeTrajectory::evaluate(double t) {
 	assert(t - startTime > 0); // TODO move to a constructor maybe
 
 	// Calculate how many full beats have passed
-	int secondsPassed = (t - startTime);
-	int beatsPassed = secondsPassed * beatsPerSecond;
-	int totalBeatsNeeded = (startTime - endTime) * beatsPerSecond;
+	int beatsPassed = (t - startTime) * beatsPerSecond;
+	int totalBeatsNeeded = (endTime - startTime) * beatsPerSecond;
 
 	// If odd or last, return the end intensity
 	// If even, return the start intensity
